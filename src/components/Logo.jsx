bf9e0e7fc38e15ca74de0354749769c7ab7d2b15
JSMATOM <jsmatom72@hotmail.com>
2025-08-19 04:18:23 +0600

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Logo = () => {
   return (
     <Link to={"/home"}>
-      <h1 className="gradient-text select-none flex text-xl">Yanime</h1>
+      <h1 className="gradient-text select-none flex text-2xl">Yanime</h1>
     </Link>
   );
 };
