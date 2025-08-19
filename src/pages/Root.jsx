44912c0fd77f24e72a55d7bc9e7490d2c7400118
JSMATOM <jsmatom72@hotmail.com>
2025-08-19 18:57:23 +0600

    Update Root.jsx

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -55,7 +55,7 @@ const Root = () => {
             <img
               src={banner}
               alt="banner"
-              className="w-64 md:w-96 lg:w-[500px] animate-fadeIn"
+              className="w-64 md:w-96 lg:w-[600px] animate-fadeIn"
             />
           </div>
 
