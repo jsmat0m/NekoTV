e63d21259d4aa3588e6e059fc1bbd8055bcf53bf
JSMATOM <jsmatom72@hotmail.com>
2025-08-23 02:03:38 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
 const colors = {
   primary: "#269cfc",
-  accent: "#a78bfa",
-  softAccent: "#b1b2ff",
-  background: "#0B1220",
-  lightbg: "#131A30",
-  card: "#131A30",
-  hoverbg: "#1C2640",
+  background: "#101B2A",
+  lightbg: "#101B2A",
+  card: "#101B2A",
+  btnbg: "#101B2A",
   yellow: "#F9ED69",
+  purple: "#B1B2FF",
   pink: "#F2BED1",
-  textPrimary: "#E0E6F1",
-  textSecondary: "#A3A9B8",
-  textTertiary: "#6C7080",
-  link: "#7DD3FC",
-  linkHover: "#A78BFA",
-  grid: "#000000",
+  lighttext: "#ccc",
 };
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
