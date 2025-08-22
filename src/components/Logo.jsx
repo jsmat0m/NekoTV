5303dc442b8c235b1cce9cf446179b82c6a57945
JSMATOM <jsmatom72@hotmail.com>
2025-08-23 00:38:24 +0600

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,7 +6,7 @@ const Logo = () => {
       <h1 className="select-none flex text-3xl font-extrabold tracking-wide">
         <span className="text-[#FF804A] drop-shadow-md">ネ</span>
         <span className="text-[#FF7F50] drop-shadow-md">コ</span>
-        <span className="text-[#FF7E57] drop-shadow-md">T</span>
+        <span className="text-[#FF3F2B] drop-shadow-md">T</span>
         <span className="text-[#FF3F2B] drop-shadow-md">V</span>
       </h1>
     </Link>
