8cf6dccfedd7e635ba8a0f1357c7e829acc657a1
JSMATOM <jsmatom72@hotmail.com>
2025-08-23 00:31:55 +0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,10 +24,10 @@ const Footer = () => {
       
       <div className="desclaimer mt-5 mb-2 flex flex-col justify-center items-center">
         <p className="text-sm text-center text-gray-500">
-          Yanime does not store any files on its servers, <br /> it only links
+          NekoTV does not store any files on its servers, <br /> it only links
           to the media hosted on third-party services.{" "}
         </p>
-        <p className="mt-4 text-gray-500">© Yanime All rights reserved.</p>
+        <p className="mt-4 text-gray-500">© NekoTV All rights reserved.</p>
         <div className="btns flex justify-center my-2 items-center gap-2">
           <a
             href="https://github.com/jsmat0m"
