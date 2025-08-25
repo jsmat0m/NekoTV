f56e1cbd5aa7c7ebf43264dd59202c17de04f6c8
JsmAtom <jsmatom72@hotmail.com>
2025-08-26 03:27:07 +0600

    Update Image.jsx

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import SoundsInfo from "../components/SoundsInfo";
 import { Link } from "react-router-dom";
 import catIcon from "../assets/cat.svg"; 
@@ -8,16 +7,14 @@ const Image = ({ data }) => {
     <div>
       <Link to={`/anime/${data.id}`}>
         <div className="film-poster group transition-all rounded-md w-full h-full pb-[140%] mb-2 relative overflow-hidden bg-[#545454] block">
-          {/* Custom Blur Overlay */}
           <div
             className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-all duration-300 z-10"
             style={{
               background: `linear-gradient(to bottom, rgba(31,79,112,0.6), rgba(76,52,140,0.5), rgba(0,0,0,0.7))`,
-              backdropFilter: "blur(4px)", // custom blur strength
+              backdropFilter: "blur(4px)",
             }}
           ></div>
 
-          {/* Custom Cat Icon (appears on hover) */}
           <div className="absolute inset-0 flex items-center justify-center z-20 opacity-0 group-hover:opacity-100 transition-all duration-300">
             <img
               src={catIcon}
@@ -26,12 +23,10 @@ const Image = ({ data }) => {
             />
           </div>
 
-          {/* Bottom Info */}
           <div className="z-30 absolute bottom-0 left-0 p-1">
             <SoundsInfo episodes={data.episodes} />
           </div>
 
-          {/* Poster Image with zoom effect */}
           <img
             className="absolute h-full w-full inset-0 object-cover object-center transition-transform duration-500 group-hover:scale-110"
             src={data.poster}
@@ -41,7 +36,6 @@ const Image = ({ data }) => {
         </div>
       </Link>
 
-      {/* Title */}
       <Link to={`/anime/${data.id}`}>
         <div
           title={data.title}
@@ -51,7 +45,6 @@ const Image = ({ data }) => {
         </div>
       </Link>
 
-      {/* Type & Duration */}
       {data.type && (
         <div className="type flex gray gap-3 items-center text-sm">
           <h4>{data.type}</h4>
